86e77f9eaf27ebe9c5d44cfea650e7a2716fd712
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-02-09 08:21:10 -0300

    em processo

--- python/aula 2.py
+++ python/aula 2.py
@@ -2,4 +2,6 @@ n1 = 15
 n2 = 7.8313
 
 print('%f + %f = %4.2f'%(n1,n2,n1+n2))
+print('%d + %d = %d'%(n1,n2,n1+n2))
 #4.2f está dizendo dos 4 digitos depois do ponto, só aparecer 2
+#para parecer as dezenas precisa usar o f 
