a5146ff6b071782d1b88ac66ae0e984772f941c3
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-02-19 20:54:53 -0300

    em processo

---
+++ python/aulas/aula3.py
@@ -0,0 +1,11 @@
+#comentario podemos usar o #
+
+
+x = int(input('Digite o primeiro valor:'))
+y = int(input('Digite o segundo valor:'))
+soma = x + y 
+prod = x * y 
+print('A soma = %d e o Produto = %d'%(soma,prod))
+
+ or we can use this form
+
