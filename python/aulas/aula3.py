534305b7aced4640173709a8c11b016a524b8b7b
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-02-19 22:11:48 -0300

    em processo

--- python/aulas/aula3.py
+++ python/aulas/aula3.py
@@ -1,11 +1,35 @@
 #comentario podemos usar o #
 
 
-x = int(input('Digite o primeiro valor:'))
-y = int(input('Digite o segundo valor:'))
-soma = x + y 
-prod = x * y 
-print('A soma = %d e o Produto = %d'%(soma,prod))
+#x = int(input('Digite o primeiro valor:'))
+#y = int(input('Digite o segundo valor:'))
+#soma = x + y 
+#prod = x * y 
+#print('A soma = %d e o Produto = %d'%(soma,prod))
+#--------------- Condições simples ----------------------------
+#valor = float(input('Então com um valor:'))
+#if valor > 0:
+#    print(valor,'É maior do que zero')
 
- or we can use this form
+# We can use this form
+
+#salario = float(input('Digite seu salário atual:'))
+#if salario > 1000:
+#    inps = salario*0.10
+#    impostoRenda = salario*0.15
+#    print('Valor do INPS:',inps,"e do imposto de Renda",impostoRenda)
+#-------------- Condições Compostas ------------------------------
+valor = int(input('Digite um valor:'))
+#if (valor >= 10)and(valor <= 20):
+#    print(valor,"Ocorre dentro do intervalo[10,20]")
+#or
+#if 10<=valor<=20:
+#    print(valor,'Ocorre dentro do intervalo[10,20]')
+#if (valor<0) or (valor>0):
+#    print(valor,'Ocorre fora do intervalo[0,30]')
+
+#ESTRUTURA DE SELEÇÃO COM DOIS RAMOS (if - else)
+ if valor > 0 :
+    print(valor,'É maior')
+
 
