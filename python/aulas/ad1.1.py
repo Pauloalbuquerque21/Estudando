cce056dc335fcdb2142a7865421146bfae50b1a7
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-02-21 12:04:53 -0300

    em processo

--- python/aulas/ad1.1.py
+++ python/aulas/ad1.1.py
@@ -1,9 +1,11 @@
 entrada = 0
-numero = 0
+
 resultado=0
 vezes=0
 media = 0
 vezes2 = 0
+maior = 0
+menor = 0
 while True:
     entrada = str(input('Digite um número:'))
     vezes = vezes + len(entrada)
@@ -13,6 +15,14 @@ while True:
             resultado = resultado + numero
             vezes2= 1 + vezes2
             media = resultado / vezes2
+            if vezes2 == 1:
+                maior = numero
+                menor = numero
+            else:
+                if numero > maior:
+                    maior = numero
+                if numero < menor:
+                    menor = numero
         except ValueError:
             print('valor não indentificado')
     else:
@@ -23,7 +33,7 @@ while True:
             break
         
         
-print('Média:',media)
+print('Média:',media,'Maior número:',maior,'Menor número:',menor)
 
 
         
