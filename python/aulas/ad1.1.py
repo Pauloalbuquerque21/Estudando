5b6fcc399f11596b3bc38db2222269877e64f0c2
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-02-21 11:51:09 -0300

    conseguindo fazer

--- python/aulas/ad1.1.py
+++ python/aulas/ad1.1.py
@@ -1,16 +1,29 @@
 entrada = 0
-escola = 0
+numero = 0
+resultado=0
+vezes=0
+media = 0
+vezes2 = 0
 while True:
-    try:
-        entrada = int(input('Digite um número:'))
-        letras = str(entrada)
-        if  len(letras) == 0:
-            print('Linha vazia lida')
-    except ValueError:
-        print('Linha vazia2')
-        if escola == 0:
+    entrada = str(input('Digite um número:'))
+    vezes = vezes + len(entrada)
+    if len(entrada) != 0:
+        try:
+            numero = int(entrada)
+            resultado = resultado + numero
+            vezes2= 1 + vezes2
+            media = resultado / vezes2
+        except ValueError:
+            print('valor não indentificado')
+    else:
+        if len(entrada)==0 and numero == 0 and vezes == 0:
+           print('Nunhum número foi lido - A primeira linha lida foi vazia')
+           break 
+        else:
             break
-
+        
+        
+print('Média:',media)
 
 
         
