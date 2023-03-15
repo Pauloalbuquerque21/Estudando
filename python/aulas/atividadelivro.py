d343a4c8ebbededf6890ec6923359a1875a107f3
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-03-15 06:38:48 -0300

    a

---
+++ python/aulas/atividadelivro.py
@@ -0,0 +1,19 @@
+test = str(input('Digite uma frase:')).split()
+print(test)
+print(test[2])
+print(len(test))
+
+
+
+
+
+
+
+
+
+
+#quantidade = int(input('Quantos textes você ira fazer?'))
+#for c in range(1,quantidade+1):
+#    palavra = str(input('Digite a paravra:'))
+
+
