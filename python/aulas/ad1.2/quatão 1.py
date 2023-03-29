6b9980db0c36540b3de43a816841844f37165b8f
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-03-29 19:10:40 -0300

    em processo

---
+++ python/aulas/ad1.2/quatão 1.py
@@ -0,0 +1,3 @@
+numperson = int(input('Favor informar o número de pessoas:'))
+for num in range (0,numperson):
+    person= str(input('Favor informa nessa ordem (Nome da pessoa, idade, 1.65.)\n obs: Sem a virgula e o ponto apenas na altura  )'))
---
+++ python/aulas/aula5.py
@@ -0,0 +1,6 @@
+#Vetores
+
+#Soma de um 
+soma = 0
+for indice in range(0,100)
+    soma = soma + itens[indice] 
