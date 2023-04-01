1b6888ea378b934bf4515ff55a5479bf94bd1ec2
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-03-31 22:26:18 -0300

    em processo

--- python/aulas/ap1/2019 q2.py
+++ python/aulas/ap1/2019 q2.py
@@ -1,3 +1,15 @@
-entrada = str(input('Digite o valor da soma:'))
+entrada = quant = 0
+while True:
+    entrada = str(input('Digite o valor da soma:'))
+    quant = len(entrada)
+    if entrada == 0:
+        break
+    else:
+        entrada1 = int(entrada)
+        dividir = entrada.split()
+
+
 
 print('-----Matriz Lida-----')
+prin(f'Valor dividido: {dividir}')
+print('---------------------')
