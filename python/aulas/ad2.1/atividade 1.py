7b2f3e173ed776a12d280241f1d6ce1d81ac12cd
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-24 23:06:06 -0300

    atividade concluida 70 porcento

--- python/aulas/ad2.1/atividade 1.py
+++ python/aulas/ad2.1/atividade 1.py
@@ -1,7 +1,14 @@
-lista = []
 quant = 0
+lista = []
 while True:
-    digito = int(input('Digite as coordenadas:'))
-    if len(digito) == 0:
+    n1 = str(input('Digite os pontos:')).strip()
+    if len(n1) == 0 and quant == 0:
+        print('Nenhum ponto foi lido - Aprimeira linha lida estava vazia')
+    elif len(n1) == 0 and quant !=0:
         break
-    print(f'Valord digitado: {digito}')
+    lista.append(n1)
+    quant = 1 + quant
+print('Listagem dos Pontos Lidos:')
+for c in range(0,len(lista)):
+    print(f'({lista[c]})\n')
+print('Fim da listagem')
