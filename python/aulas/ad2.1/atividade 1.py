e597b33551d4f50d12975dbf324a40c6c6bd9ef9
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-04-24 23:04:11 -0300

    testando

--- python/aulas/ad2.1/atividade 1.py
+++ python/aulas/ad2.1/atividade 1.py
@@ -1,12 +1,7 @@
 lista = []
-quant = n1 = 0
+quant = 0
 while True:
-    n1 = input('Digite as coordenadas:')
-    if quant == 0 and n1 == 0:
-        print('Nenhum conto foi lido - A primeira linha lida está vazia')
-    elif quant != 0 and n1 == 0:
+    digito = int(input('Digite as coordenadas:'))
+    if len(digito) == 0:
         break
-    lista.append(n1)
-    quant = 1 + quant
-print(f'Número de vezez: {quant}')
-print(f'os elementos {lista}')
+    print(f'Valord digitado: {digito}')
