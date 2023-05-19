d094fd6db98abf4fdd8521154d2051c346eb6015
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-05-19 18:56:48 -0300

    Update ad2.py

--- python/ad2.2/ad2.py
+++ python/ad2.2/ad2.py
@@ -4,5 +4,11 @@
 
 
 #with open(nome do arquivo,o que você quer fazer) as tranforma essa ação para a variavel "arquivo"
-with open('email.txt','r') as arquivo:
-    arquivo.read()
+# read -> para arquivos simples (ex: senhas,tabelas, informações únicas)
+#with open("mensagem.txt","r", encoding = 'utf B') as arquivo:
+#    email = arquivo.read()
+#print(f"{email}")
+#readlines -> para arquivos maiores
+#with open("mensagem.txt","r") as arquivos:
+#    mensagem = arquivos.readlines()
+#print(mensagem)
