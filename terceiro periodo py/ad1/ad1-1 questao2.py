afcf4fef963829b77d489419f0be25fddba9a2f5
Pauloalbuquerque21 <84546934+Pauloalbuquerque21@users.noreply.github.com>
2023-08-09 08:59:55 -0300

    i try resolver this problem

--- terceiro periodo py/ad1/ad1-1 questao2.py
+++ terceiro periodo py/ad1/ad1-1 questao2.py
@@ -1,22 +1,22 @@
 pares = quant = impares = quantpares = quantimpares = maior = menor = 0
 while True:
-    quant += 1
-    valores = input(f'Digite {quant}° número inteiros:')
-    if len(valores) == 0:
-        break
-    if quant == 1:
-        maior = valores
-        menor = valores
-    else:
-        if valores > maior:
+    try:
+        quant += 1
+        valores = int(input(f'Digite {quant}° número inteiros:'))
+        if quant == 1:
             maior = valores
-        if valores < menor:
             menor = valores
-    if valores % 2 == 0:
-        pares = valores + pares
-        quantpares += 1
-    else:
-        impares = valores + impares
-        quantimpares += 1
-
-print(f'Menor:{menor}\nMaior{maior}\nMédia dos pares:{pares/quantpares:.2f}\nMédia dos impares:{impares/quantimpares:.3f}')
+        else:
+            if valores > maior:
+                maior = valores
+            if valores < menor:
+                menor = valores
+        if valores % 2 == 0:
+            pares = valores + pares
+            quantpares += 1
+        else:
+            impares = valores + impares
+            quantimpares += 1
+        print(f'Menor:{menor}\nMaior{maior}\nMédia dos pares:{pares / quantpares:.2f}\nMédia dos impares:{impares / quantimpares:.3f}')
+    except:
+        break
