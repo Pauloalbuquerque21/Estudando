c1961fca97efcff65c9a8697cfa462fdc8194d68
Pauloalbuquerque21 <84546934+Pauloalbuquerque21@users.noreply.github.com>
2023-08-10 09:23:15 -0300

    em processo

---
+++ terceiro periodo py/ad1/ad1-1 questao3.py
@@ -0,0 +1,8 @@
+valores = input('Digite os números:')
+palavra = valores.split()
+quanti = valores.strip().replace(" ","")
+print(palavra)
+print(valores)
+#for c in range(0,len(valores)):
+#numeros = int(palavra[0])
+#print(numeros)
