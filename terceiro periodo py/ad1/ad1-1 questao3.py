ff5f9e03c270e67316da2b59c6e38ffca3bea8f0
Pauloalbuquerque21 <84546934+Pauloalbuquerque21@users.noreply.github.com>
2023-08-11 09:25:44 -0300

    em processo

--- terceiro periodo py/ad1/ad1-1 questao3.py
+++ terceiro periodo py/ad1/ad1-1 questao3.py
@@ -1,8 +1,23 @@
+numeros = maior = menor = impares = pares = quantpares = quantimpares = 0
 valores = input('Digite os números:')
-palavra = valores.split()
-quanti = valores.strip().replace(" ","")
-print(palavra)
-print(valores)
-#for c in range(0,len(valores)):
-#numeros = int(palavra[0])
-#print(numeros)
+#palavra = valores.split()
+#quanti = valores.strip()
+test = valores.replace(' ','')
+print(test)
+print(len(test))
+for c in range(0,len(test)):
+    numeros = int(test[c])
+    if len(test) == 1:
+        maior = numeros
+        menor = numeros
+    else:
+        if numeros > maior:
+            maior = numeros
+        if numeros < menor:
+            menor = numeros
+    if valores % 2 == 0:
+        pares = numeros + pares
+        quantpares += 1
+    else:
+        impares = numeros + impares
+        quantimpares += 1
