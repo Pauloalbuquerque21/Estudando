715d468d6f9e708efad9c7ea8bcd9a5b0e06e1cc
Pauloalbuquerque21 <84546934+Pauloalbuquerque21@users.noreply.github.com>
2023-08-15 23:03:05 -0300

    em processo

--- terceiro periodo py/ad1/ad1-1 questao3.py
+++ terceiro periodo py/ad1/ad1-1 questao3.py
@@ -15,9 +15,11 @@ for c in range(0,len(test)):
             maior = numeros
         if numeros < menor:
             menor = numeros
-    if valores % 2 == 0:
+    if numeros % 2 == 0:
         pares = numeros + pares
         quantpares += 1
     else:
         impares = numeros + impares
         quantimpares += 1
+
+print(f'Menor:{menor}\nMaior{maior}\nMédia dos pares:{pares/quantpares:.2f}\nMédia dos impares:{impares/quantimpares:.3f}')
