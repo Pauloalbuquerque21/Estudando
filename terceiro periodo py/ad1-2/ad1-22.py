38f189423c2dcbda44e430e5158465893b693075
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2023-08-31 09:23:38 -0300

    em processo

--- terceiro periodo py/ad1-2/ad1-22.py
+++ terceiro periodo py/ad1-2/ad1-22.py
@@ -1,17 +1,7 @@
-dado = list()
-quant = 0
-tamanho = int (input('Qual o tamanho da permutação:'))
-while True:
-    inf = int(input(f'Digite o {quant+1}° valor:'))
-    if inf in dado:
-        print('Valor digitádo já existe na permutação, favor tente novamente!')
-    else:
-        dado.append(inf)
-        quant+=1
-    if quant == tamanho:
-         break
-print(dado)
-def redece(tamanho):
+permutacao = dado = list()
+test = 0
+def recebe(tamanho):
+    quant = 0
     while True:
         inf = int(input(f'Digite o {quant + 1}° valor:'))
         if inf in dado:
@@ -21,3 +11,17 @@ def redece(tamanho):
             quant += 1
         if quant == tamanho:
             break
+    return dado
+
+
+recebe(int(input('Digite o valor da permutação')))
+print(dado)
+while True:
+    if len(permutacao) == 0:
+        permutacao.append(dado[0])
+    else:
+        test = len(permutacao)
+        permutacao.append(dado[test])
+    if len(permutacao) == tamanho:
+        break
+print(f'{dado} e \n{permutacao}')
