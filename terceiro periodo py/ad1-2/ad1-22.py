504ca4e33cc4459af382276a354442855c091fb5
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2023-09-03 20:42:10 -0300

    quase terminando

--- terceiro periodo py/ad1-2/ad1-22.py
+++ terceiro periodo py/ad1-2/ad1-22.py
@@ -1,13 +1,14 @@
 permutacao = list()
 dado = list()
-test = 0
+test = 1
+inf = quant = 0
 def recebe(tamanho):
     quant = 0
     while True:
         inf = int(input(f'Digite o {quant + 1}° valor:'))
         if inf in dado:
             print('Valor digitádo já existe na permutação, favor tente novamente!')
-        elif inf >= tamanho:
+        elif inf > tamanho:
             print('número muito alto')
         elif inf == 0:
             print('Favor não digite zero')
@@ -17,16 +18,25 @@ def recebe(tamanho):
         if quant == tamanho:
             break
     return dado
-
-
-recebe(int(input('Digite o valor da permutação')))
+recebe(int(input('Digite o tamanho da permutação:')))
 print(f'{dado}')
+
 while True:
     if len(permutacao) == 0:
+        permutacao.append(1)
         permutacao.append(dado[0])
+        print(permutacao)
     else:
-        test = len(permutacao)
-        permutacao.append(dado[test])
+        print(permutacao)
+        test = test + 1
+        print(f'test: {test}')
+        inf = permutacao[test-1]
+        print(f'inf: {inf}')
+        if dado[inf-1] in permutacao:
+            test = 1 + quant
+            quant = quant + 1
+        else:
+            permutacao.append(dado[inf-1])
     if len(permutacao) == len(dado):
         break
 print(f'{dado} e \n{permutacao}')
