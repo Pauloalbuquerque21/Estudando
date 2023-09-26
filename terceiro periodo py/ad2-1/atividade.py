cefcdf8e6c64eaef8d3eaf0f2543e340957a9e15
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2023-09-26 08:08:55 -0300

    fazendo a atividade ad2-1

--- terceiro periodo py/ad2-1/atividade.py
+++ terceiro periodo py/ad2-1/atividade.py
@@ -1,11 +1,26 @@
-quantidade_linha = 0
+quantidade_linha = information_matriz = 0
 dados_matriz = list()
-while True:
-    information_matriz.clean()
-    quantidade_linha += 1
-    information_matriz = input(f'Linhas {quantidade_linha}:')
-    if information_matriz == 0:
-        break
-    else:
+dados_das_matrizes = list()
+for c in range(0,1):
+    quantidade_linha = 0
+    while True:
+        quantidade_linha += 1
+        information_matriz = input(f'Matriz {c} Linhas {quantidade_linha}:')
         dados_divididos = information_matriz.split()
-        dados_matriz.append(dados_divididos)
+        print(dados_divididos)
+        print(quantidade_linha)
+        if len(information_matriz) == 0:
+            dados_das_matrizes.append(quantidade_linha)
+            break
+        else:
+            if quantidade_linha > 1:
+                if len(dados_matriz[quantidade_linha-2]) != len(dados_divididos):
+                    quantidade_linha -= 1
+                    print('Fazor digitar o mesmo número de colunas que matriz anterior ')
+                else:
+                    dados_matriz.append(dados_divididos)
+            else:
+                dados_matriz.append(dados_divididos)
+print(dados_matriz)
+print(len(dados_matriz[0]))
+print(dados_de_matrizes)
