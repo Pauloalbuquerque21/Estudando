3d74f052ef2823a57568256714e45b50e2f1a322
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2023-09-30 11:08:34 -0300

    terminei a parte de criação de matrizes

--- terceiro periodo py/ad2-1/atividade.py
+++ terceiro periodo py/ad2-1/atividade.py
@@ -1,16 +1,20 @@
-quantidade_linha = information_matriz = n1 = 0
+quantidade_linha = information_matriz = 0
+dados_das_matrizes =  dados_divididos_num = list()
 dados_matriz = list()
-dados_das_matrizes = list()
 for c in range(0,2):
     print(c)
-
     quantidade_linha = 0
     while True:
         quantidade_linha += 1
         information_matriz = input(f'Matriz {c} Linhas {quantidade_linha}:')
-        dados_divididos = information_matriz.split()
-        print(dados_divididos)
+        dados_divididos_str = information_matriz.split()
+        print(dados_divididos_str)
+        dados_divididos_num = list()
+        for c in range(0,len(dados_divididos_str)):
+            dados_divididos_num.append(int(dados_divididos_str[c]))
+        print(dados_divididos_num)
         print(quantidade_linha)
+        print(dados_das_matrizes)
         if len(information_matriz) == 0:
             dados_das_matrizes.append(quantidade_linha-1)
             break
@@ -29,10 +33,9 @@ for c in range(0,2):
                 #else:
                 #    dados_matriz.append(dados_divididos)
             #else:
-            transform_num = int(dados_divididos)
-            dados_matriz.append(transform_num)
-print(dados_matriz)
-print(dados_das_matrizes)
+            dados_matriz.append(dados_divididos_num)
+print(f'Dados_matrizes:{dados_matriz}')
+print(f'Dados_das_matrizes:{dados_das_matrizes}')
 
 def leituraDeMatriz(dado):
     tamanho_matriz_a = dados_das_matrizes[0]
@@ -40,12 +43,12 @@ def leituraDeMatriz(dado):
     matriz_A = matriz_B = list()
     if dado in 'aA':
         for c in range(0,tamanho_matriz_a):
-            for c2 in range(0,len(dados_matriz[0])):
-                numb = int(dados_matriz[c][c2])
-                matriz_A.append(numb)
-        for c in range(0, tamanho_matriz_a):
-            for c2 in range(0,):
-    return matriz_A
-
-valoresA = leituraDeMatriz('a')
+            matriz_A.append(dados_matriz[c])
+        return matriz_A
+    if dado in 'bB':
+        for c in range(tamanho_matriz_a,len(dados_matriz)):
+            matriz_A.append(dados_matriz[c])
+        return matriz_B
+escolha_matriz = input('Escolha a matriz desejada:')
+valoresA = leituraDeMatriz(escolha_matriz)
 print(valoresA)
