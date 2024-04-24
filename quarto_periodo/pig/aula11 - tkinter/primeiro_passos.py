0bf0b0b309a3112e2893991b577c55d25542087c
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-04-24 19:01:01 -0300

    aprendendo tkinteter

--- quarto_periodo/pig/aula11 - tkinter/primeiro_passos.py
+++ quarto_periodo/pig/aula11 - tkinter/primeiro_passos.py
@@ -1,7 +1,20 @@
 from tkinter import *
 
+def botao_clicado():
+    a.config(text='Botao Clicar')
 janela_principal = Tk()
-label = Label(janela_principal, text = "Ola mundo")
-label.pack()
+#titulo
+janela_principal.title('Estudando Tkinter')
+#informação dentro do programa
+a = Label(janela_principal, text = "Ola mundo")
+a.pack(side="left")
+
+#informação dentro do programa
+b = Label(janela_principal, text = "Ola mundo")
+b.pack(side="left")
+
+
+botao = Button(janela_principal, text="Clique aqui", command = botao_clicado )
+botao.pack()
 
 janela_principal.mainloop()
