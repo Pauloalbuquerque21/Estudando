f1ebcedb6723f9c496102d500fdd67177d138c7a
Pauloalbuquerque21 <paulo.vitor21@hotmail.com>
2024-04-26 20:58:32 -0300

    apreusando o tkinter

--- quarto_periodo/pig/aula11 - tkinter/primeiro_passos.py
+++ quarto_periodo/pig/aula11 - tkinter/primeiro_passos.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 def botao_clicado():
     a.config(text='Botao Clicar')
+    b.config(text='Botão clicar+++')
 janela_principal = Tk()
 #titulo
 janela_principal.title('Estudando Tkinter')
@@ -9,12 +10,19 @@ janela_principal.title('Estudando Tkinter')
 a = Label(janela_principal, text = "Ola mundo")
 a.pack(side="left")
 
-#informação dentro do programa
-b = Label(janela_principal, text = "Ola mundo")
-b.pack(side="left")
+#Frame com alguns parametros
+frame_superior = Frame(janela_principal, borderwidth=2, relief = "groove")
+frame_superior.pack()
 
+#informação dentro do programa b
+b = Label(janela_principal, text = "Ola mundo")
+b.pack(side="right")
+
+#informação dentro do programa c
+c = Label(janela_principal, text = "Ola mundo")
+c.pack(side="top")
 
 botao = Button(janela_principal, text="Clique aqui", command = botao_clicado )
-botao.pack()
+botao.pack(side="top")
 
 janela_principal.mainloop()
